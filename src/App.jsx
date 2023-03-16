62c73341549e48b9bfa8939d897a040d68d7f7b1
Romeo Kiss <kiss.romeo.richard@gmail.com>
2023-03-16 14:45:15 +0200

    Added the tip calculation and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,143 +1,179 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
+import classNames from "classnames";
 import { Header } from './components/Header';
+
 
 function App() {
   const [data, setData] = useState({
     bill: 0,
-    // tip: 0,
     customTip: 0,
-    persons: 1
+    persons: 0
   });
 
-  function handleChange(event) {
+  const [placeholder, setPlaceHolder] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [activeButtonIndex, setActiveButtonIndex] = useState(null);
+  const [tip, setTip] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const hasZeroValue = Object.values(data).some((value) => value !== 0);
+    setIsDisabled(hasZeroValue);
+  }, [data]);
+
+  const Reset = () => {
+    setPlaceHolder(0);
+    setData({
+      bill: 0,
+      customTip: 0,
+      persons: 0
+    });
+    setIsDisabled(true);
+  };
+
+  const handleInputChange = (event) => {
     const { value, name } = event.target;
 
     setData((previousValue) => {
-      if (name === 'bill') {
-        return {
-          bill: value,
-          // tip: previousValue.tip,
-          customTip: previousValue.customTip,
-          persons: previousValue.persons
-        };
-      } else if (name === 'customTip') {
-        return {
-          bill: previousValue.bill,
-          // tip: previousValue.tip,
-          customTip: value,
-          persons: previousValue.persons
-        };
-      } else if (name === 'persons') {
-        return {
-          bill: previousValue.bill,
-          // tip: previousValue.tip,
-          customTip: previousValue.customTip,
-          persons: value
-        };
-      }
+      return {
+        ...previousValue,
+        [name]: value,
+      };
     });
-  }
-  const buttons = [5, 10, 15, 25, 50];
+  };
+
+  const tipCalculator = (inputTip) => {
+    if (data.persons > 0) {
+      let totalPerPerson = (data.bill * (inputTip / 100)) / data.persons;
+      totalPerPerson = totalPerPerson.toFixed(2);
+      let totalAmount = (data.bill / data.persons) + Number(totalPerPerson);
+      totalAmount = totalAmount.toFixed(2);
+      setTip(totalPerPerson);
+      setTotal(totalAmount);
+    }
+  };
+
+  const handleClickBtn = (tipButtonValue, index) => {
+    tipCalculator(tipButtonValue);
+    setActiveButtonIndex(index);
+  };
+  const tipButtonsValues = [5, 10, 15, 25, 50];
 
   return (
-    <>
-      <div className="page page__body">
-        <Header />
-        <main>
-          <div className="page__section amount">
-            <div className="container">
-              <div className="amount__in">
-                <div className="input__container">
+
+    <div className="page page__body">
+      <Header />
+      <main>
+        <div className="page__section amount">
+          <div className="container">
+            <div className="amount__in">
+              <div className="input__container">
+                <div className="error__container">
+
                   <label
                     htmlFor="bill"
                     className="input__label"
                   >
                     Bill
                   </label>
-                  <input
-                    className="input__data input__data-bill"
-                    type="number"
-                    name="bill"
-                    id="bill"
-                    placeholder="0"
-                    value={data.bill}
-                    required
-                    onChange={handleChange}
-                  />
+                  {!data.bill &&
+                    <span className="input__label-error">Can&apos;t be zero</span>
+                  }
                 </div>
-                <div className="input__container">
-                  <div className="input__label input__label-select">Select Tip %</div>
-                  <div className="buttons-container">
-                    {buttons.map((button, index) => (
-                      <button
-                        className="button button__amount"
-                        key={index}
-                        value={button / 100}
-                      >
-                        {button}%
-                      </button>
-                    ))}
-                    <input
-                      type="number"
-                      name='customTip'
-                      placeholder="Custom"
-                      className="input__data input__data-custom"
-                      value={data.customTip}
-                      onChange={handleChange}
-                    />
-                  </div>
-                </div>
-                <div className="input__container">
-                  <div className="error__container">
-                    <label
-                      htmlFor="persons"
-                      className="input__label"
+                <input
+                  className="input__data input__data-bill"
+                  type="number"
+                  name="bill"
+                  id="bill"
+                  placeholder={placeholder}
+                  inputMode="decimal"
+                  value={data.bill}
+                  required
+                  onChange={handleInputChange}
+                  invalid={data.bill}
+                />
+              </div>
+              <div className="input__container">
+                <div className="input__label input__label-select">Select Tip %</div>
+                <div className="buttons-container">
+                  {tipButtonsValues.map((tipButtonValue, index) => (
+                    <button
+                      className={classNames('button', 'button__amount', {
+                        "button__amount-active": index === activeButtonIndex,
+                      })}
+                      key={index}
+                      value={tipButtonValue}
+                      onClick={() => handleClickBtn(tipButtonsValues[index], index)}
                     >
-                      Number of People
-                    </label>
-                    <span className="input__label-error">Can’t be zero</span>
-                  </div>
+                      {tipButtonValue}%
+                    </button>
+                  ))}
                   <input
                     type="number"
-                    name="persons"
-                    id="num_people"
-                    placeholder="0"
-                    required
-                    className="input__data input__data-persons"
-                    value={data.persons}
-                    onChange={handleChange}
+                    name='customTip'
+                    placeholder="Custom"
+                    inputMode="decimal"
+                    className="input__data input__data-custom"
+                    value={data.customTip}
+                    onChange={handleInputChange}
                   />
                 </div>
               </div>
-              <div className="amount__out">
-                <div className="amount__out-container">
-                  <div className="amount__info-container">
-                    <div className="amount__info ">
-                      <div className="amount__info-title">Tip Amount</div>
-                      <div className="amount__info-person">/ person</div>
-                    </div>
-                    <div className="amount__info-perPerson">$0.00</div>
-                  </div>
-                  <div className="amount__info-container">
-                    <div className="amount__info ">
-                      <div className="amount__info-title">Total</div>
-                      <div className="amount__info-person">/ person</div>
-                    </div>
-                    <div className="amount__info-total">$0.00</div>
-                  </div>
+              <div className="input__container">
+                <div className="error__container">
+                  <label
+                    htmlFor="persons"
+                    className="input__label"
+                  >
+                    Number of People
+                  </label>
+                  {!data.persons &&
+                    <span className="input__label-error">Can&apos;t be zero</span>
+                  }
                 </div>
-                <button
-                  className="button button__reset"
-                  disabled
-                >Reset</button>
+                <input
+                  type="number"
+                  name="persons"
+                  id="num_people"
+                  placeholder={placeholder}
+                  required
+                  className="input__data input__data-persons"
+                  value={data.persons}
+                  onChange={handleInputChange}
+                />
               </div>
             </div>
-
+            <div className="amount__out">
+              <div className="amount__out-container">
+                <div className="amount__info-container">
+                  <div className="amount__info ">
+                    <div className="amount__info-title">Tip Amount</div>
+                    <div className="amount__info-person">/ person</div>
+                  </div>
+                  <div className="amount__info-perPerson">${tip}</div>
+                </div>
+                <div className="amount__info-container">
+                  <div className="amount__info ">
+                    <div className="amount__info-title">Total</div>
+                    <div className="amount__info-person">/ person</div>
+                  </div>
+                  <div className="amount__info-total">${total}</div>
+                </div>
+              </div>
+              <button
+                className="button button__reset"
+                disabled={!isDisabled}
+                onClick={() => Reset()}
+              >
+                Reset
+              </button>
+            </div>
           </div>
-        </main>
-      </div>
-    </>
+
+        </div>
+      </main>
+    </div>
   );
 }
 
